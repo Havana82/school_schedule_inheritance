b2eeb6fe7a1e7cdc47e3de7ec7e67ff1378fb174
Whitney <whitneyshake@gmail.com>
2023-04-05 11:24:15 -0700

    MiddleSchoolStudent Class Practice

--- school_schedule/middle_school_student.py
+++ school_schedule/middle_school_student.py
@@ -1,3 +1,16 @@
 from .student import Student
 
 # add MiddleSchoolStudent here
+class MiddleSchoolStudent(Student):
+    def __init__(self, name, grade, classes, get_transportation=False):
+        super().__init__(name, grade, classes)
+        self.get_transportation = get_transportation
+
+    def summary(self):
+        student_summary = super().summary
+        get_message = "gets" if self.get_transportation else "doesn't get"
+        return (f"{student_summary}"
+                f"{self.name} {get_message} transportation")
+    
+John = MiddleSchoolStudent("John", "7th", ["Art", "Science"])
+print(John.summary())
